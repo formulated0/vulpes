3a69c7b20fe619361568ae5e11609754d7604a94
formulated0 <168610227+formulated0@users.noreply.github.com>
2026-03-16 15:49:38 +1100

    read input and break on EOF

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use std::io::Write;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
 	loop {
 		print!("$ ");
 		std::io::stdout().flush().unwrap();
 		let mut input = String::new();
-		io::stdin().read_line(&mut input).expect("failed to read line");
-		print!("{input}")
-		// std::process::exit()
+		let bytes = io::stdin().read_line(&mut input).expect("failed to read line");
+		
+		if bytes == 0 {
+			break
+		}
+        
+		print!("{}", input)
 	}
 }
